80ef073d99bd7dd60950cf8b6b22afec3c59916e
Mohammad Mostafizur Rahman <mrmostafizurrhaman@gmail.com>
2026-09-11 15:39:21 +0600

    done

--- backend/obj/Debug/net9.0/KeyaExportImport.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/KeyaExportImport.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KeyaExportImport.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6348f86c1b915da4d0e46aebdcc1d71c17fbd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c91fb2e912c1763f90fcd620ed79ccf778971b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeyaExportImport.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeyaExportImport.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
